5677027037985d33ff9f97bec3094804001ee590
mnagarat <munis36@hotmail.com>
2018-01-06 21:53:19 -0500

    lambda

---
+++ src/main/java/com/munis/lambda/Hello.java
@@ -0,0 +1,10 @@
+package com.munis.lambda;
+
+/**
+ * Created by mnagarat on 1/6/18.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("helloworld");
+    }
+}
